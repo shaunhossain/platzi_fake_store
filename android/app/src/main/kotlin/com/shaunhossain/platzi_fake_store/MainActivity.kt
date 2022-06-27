0cf9bf777b20817e892ae95519e587f69b45b8ab
shaunhossain <shaunhossain95@gmail.com>
2022-06-27 17:50:47 +0600

    project initialization

---
+++ android/app/src/main/kotlin/com/shaunhossain/platzi_fake_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shaunhossain.platzi_fake_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/endpoint.dart
@@ -0,0 +1,1 @@
+const String baseUrl = "https://fakeapi.platzi.com/doc/auth-jwt";
