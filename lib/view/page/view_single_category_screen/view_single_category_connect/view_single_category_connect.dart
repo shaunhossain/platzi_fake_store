cfb434e23f0f79489073c20b63bfe1d8fbf3f56e
shaun <shaunhossain95@gmail.com>
2022-07-05 00:26:58 +0600

    add gridview and fetch product data from server

--- lib/view/page/view_single_category_screen/view_single_category_connect/view_single_category_connect.dart
+++ lib/view/page/view_single_category_screen/view_single_category_connect/view_single_category_connect.dart
@@ -5,30 +5,16 @@ import 'package:platzi_fake_store/utils/endpoint.dart';
 
 
 abstract class ViewSingleCategoryProvider {
-  Future<dynamic> getAllProductCategory();
-  Future<dynamic> getAllProducts();
+  Future<dynamic> getAllProductOfCategory({required int categoryId});
 }
 
 class IViewSingleCategoryProvider extends GetConnect implements ViewSingleCategoryProvider {
 
   @override
-  Future getAllProductCategory() async {
+  Future getAllProductOfCategory({required int categoryId}) async {
 
     final response = await http.get(
-      Uri.parse(baseUrl+allCategoryEndpoint),
-    );
-    if (response.statusCode == 200) {
-      log(response.body);
-      return response.body;
-    } else {
-      return response.body;
-    }
-  }
-
-  @override
-  Future getAllProducts() async {
-    final response = await http.get(
-      Uri.parse(baseUrl+allProductEndpoint),
+      Uri.parse(baseUrl+singleCategoryProductsEndpoint+categoryId.toString()+addedSingleCategoryProductsEndpoint),
     );
     if (response.statusCode == 200) {
       log(response.body);
--- lib/view/page/view_single_category_screen/view_single_category_screen.dart
+++ lib/view/page/view_single_category_screen/view_single_category_screen.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:platzi_fake_store/model/category/category.dart';
+import 'package:platzi_fake_store/utils/size_config.dart';
+import 'package:platzi_fake_store/view/components/navigator/app_pages.dart';
+import 'package:platzi_fake_store/view/components/widget/custom_text_view.dart';
+import 'package:platzi_fake_store/view/components/widget/home/product_view_item.dart';
+import 'package:platzi_fake_store/view/page/view_single_category_screen/view_single_category_controller/view_single_category_controller.dart';
 
-class ViewSingleCategoryScreen extends StatelessWidget {
+class ViewSingleCategoryScreen extends StatefulWidget {
   const ViewSingleCategoryScreen({Key? key}) : super(key: key);
 
   @override
+  State<ViewSingleCategoryScreen> createState() =>
+      _ViewSingleCategoryScreenState();
+}
+
+class _ViewSingleCategoryScreenState extends State<ViewSingleCategoryScreen> {
+  final Category category = Get.arguments;
+
+  @override
+  initState() {
+    super.initState();
+    final controller = Get.find<ViewSingleCategoryController>();
+    controller.getAllProductsOfCategory(categoryId: category.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: Text("view single category product"),
-          ),
-        ));
+    SizeConfig().init(context);
+    return GetBuilder<ViewSingleCategoryController>(builder: (controller) {
+      return SafeArea(
+          child: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          title: CustomTextView(
+              text: category.name,
+              fontSize: SizeConfig.textScaleFactor! * 24,
+              fontWeight: FontWeight.w500,
+              color: Colors.black),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Get.toNamed(AppRoutes.searchProductScreen);
+                },
+                icon: const Icon(Icons.search_rounded))
+          ],
+          backgroundColor: Colors.white,
+          bottomOpacity: 0.0,
+          elevation: 0.3,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 12, right: 12, top: 8),
+          child: Obx(()=> controller.allProductOfCategory.isEmpty
+              ? const Center(
+            child: CircularProgressIndicator(),
+          )
+              : GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 20,
+                childAspectRatio: 0.79,),
+              itemCount: controller.allProductOfCategory.length,
+              itemBuilder: (BuildContext ctx, index) {
+                return ProductViewItem(
+                  productItem: controller.allProductOfCategory[index],
+                  onTap: () {
+                    Get.toNamed(AppRoutes.viewProductScreen);
+                  },
+                );
+              }),),
+        )
+      ));
+    });
   }
 }
