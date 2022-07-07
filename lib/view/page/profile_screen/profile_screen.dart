63f80808a61d312ef3312791ad89b86e37fa0313
shaunhossain <shaunhossain95@gmail.com>
2022-07-07 14:18:14 +0600

    add database operation logic

--- lib/view/page/profile_screen/profile_screen.dart
+++ lib/view/page/profile_screen/profile_screen.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:platzi_fake_store/utils/size_config.dart';
+import 'package:platzi_fake_store/view/components/widget/custom_text_view.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text('profile'),
+    SizeConfig().init(context);
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          title: CustomTextView(
+              text: "Profile",
+              fontSize: SizeConfig.textScaleFactor! * 24,
+              fontWeight: FontWeight.w500,
+              color: Colors.black),
+          backgroundColor: Colors.white,
+          bottomOpacity: 0.0,
+          elevation: 0.3,
+        ),
       ),
     );
   }
